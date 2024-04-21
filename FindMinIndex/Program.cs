ef6f7eb2944e04fb424fc1a323849a0dcca9f99b
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-22 01:45:41 +0700

    Program.cs

--- FindMinIndex/Program.cs
+++ FindMinIndex/Program.cs
@@ -1,4 +1,4 @@
-﻿int []array = {2,3,4,5,6};
+int []array = {2,3,4,5,6};
 int index = MinInArray(array);
 System.Console.WriteLine("Số nhỏ nhất trong mảng là: " + index);
 
@@ -7,12 +7,12 @@ static int MinInArray(int []array)
     int min = array[0];
     int index = 0;
 
-    for(int i = 0 ; i < array.Length; i++)
+    for(int i = 1 ; i < array.Length; i++)
     {
         if(array[i] < min)
         {
             min = array[i];
-            index = min;
+            index = i;
         }
     }
 
