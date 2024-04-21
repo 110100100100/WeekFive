bcec5136f1af61c4d3092d1cc0adbac85a567657
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-21 23:40:23 +0700

    Add files via upload

---
+++ FindMin/Program.cs
@@ -0,0 +1,20 @@
+﻿int []arr = {4, 12, 7, 8, 1, 6, 9};
+int index = FindMinValueInArray(arr);
+System.Console.WriteLine("Số nhỏ nhất trong mảng là : " + arr[index]);
+
+static int FindMinValueInArray(int[] array)
+{
+    int min = array[0];
+    int index = 0;
+
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+
+    return index;
+}
